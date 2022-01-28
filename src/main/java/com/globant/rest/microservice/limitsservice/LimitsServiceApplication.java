99ed4bd55292b8c75636ba4787cffa1ccd39b1da
cbarros <camilo.barros@globant.com>
2022-01-28 13:24:11 -0300

    Initial commit

---
+++ src/main/java/com/globant/rest/microservice/limitsservice/LimitsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.globant.rest.microservice.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+
+}
