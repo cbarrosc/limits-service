4e416b33ebdd0e103a19d52086ac0152eb7c11a4
cbarros <camilo.barros@globant.com>
2022-01-28 13:58:04 -0300

    added configuration properties and configuration bean

--- src/main/java/com/globant/rest/microservice/limitsservice/controller/LimitsController.java
+++ src/main/java/com/globant/rest/microservice/limitsservice/controller/LimitsController.java
@@ -1,15 +1,20 @@
 package com.globant.rest.microservice.limitsservice.controller;
 
 import com.globant.rest.microservice.limitsservice.bean.Limits;
+import com.globant.rest.microservice.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits(){
-        return new Limits(1,100);
+        return new Limits(configuration.getMinimum(),configuration.getMaximum());
     }
 
 }
